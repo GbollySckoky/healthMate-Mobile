18a0df9f3990179c2796d869f4a7d74ff7992a96
Gbolly <gbolahancoker99@gmail.com>
2025-08-24 16:34:15 +0100

    worked on streak interface in home page

--- app/home/_components/Streak.tsx
+++ app/home/_components/Streak.tsx
@@ -1,18 +1,26 @@
 import {
-  CardTitle,
   SubTitle,
 } from '@/components/typography/Typography';
 import {
   View,
   StyleSheet,
+  Text,
+  Image
 } from 'react-native';
 
 const Streak = () => {
+  const profile = require('../../../assets/images/Group 19153.png')
   return (
     <View style={style.SubCard}>
       <SubTitle>3 days Streak!</SubTitle>
       <SubTitle>Consistency is key – you're doing great!</SubTitle>
-      <CardTitle>Every log brings you closer to better health.</CardTitle>
+      <Text style={style.CardTitle}>Every log brings you closer to better health.</Text>
+      <Image 
+        source={profile}
+        alt="Profile Image"
+        style={style.backgroundImage}
+        resizeMode="contain"
+      />
     </View>
   );
 };
@@ -22,7 +30,7 @@ export default Streak;
 export const style = StyleSheet.create({
   SubCard: {
     padding: 15,
-    borderColor: '#F1F1F1',
+    borderColor: '#BAAEED',
     borderWidth: 1,
     borderRadius: 10,
     backgroundColor: '#F4ECFF',
@@ -34,5 +42,20 @@ export const style = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 3,
     marginTop: 15,
+    position: 'relative'
+  },
+  backgroundImage: {
+    position: 'absolute',
+    right: 0,
+    width: 100,
+    height: 100,
+  },
+  CardTitle: {
+    fontFamily: 'Inter_400Regular',
+    fontSize: 12,
+    color: '#414651',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    marginTop: 3
   },
 });
